051684eee4d63c20ed54f7ed701cb18562c0c560
Bhargava Krishna Marripati <marripatibhargava@gmail.com>
2020-04-30 17:09:19 +0530

    Add files via upload

---
+++ Linear_Regression_Math_Class/Student_LinearRegression_ML.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv("student-mat.csv", sep=";")
+
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences", "traveltime", "famrel", "freetime",
+             "goout", "Dalc", "Walc", "health"]]
+
+predict = "G3"
+
+X = np.array(data.drop([predict], 1)) # Features
+y = np.array(data[predict]) # Labels
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+acc = linear.score(x_test, y_test)
+print(acc)
+
+print('Coefficient: \n', linear.coef_)
+print('Intercept: \n', linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
